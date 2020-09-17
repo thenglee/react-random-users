72303f1a9eebe5cc8a9850a1de08828c3e261b20
Tan Theng Lee <thengleetan@gmail.com>
2020-09-17 23:05:27 +0800

    Use env var PORT and prepare script for heroku deployment

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const axios = require('axios')
 const path = require('path')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/', (req, res) => {
@@ -15,6 +17,6 @@ app.get('/users', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server started on port 3000')
 })
