619c865874e3fb2155296825231775c1559384a8
Tan Theng Lee <thengleetan@gmail.com>
2020-09-17 16:32:32 +0800

    Access server api from react app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 
 class App extends React.Component {
+  componentDidMount() {
+    axios.get('http://localhost:3000/users').then(response => {
+      console.log(response.data)
+    })
+  }
+
   render() {
     return <h1>Welcome to react!</h1>
   }
